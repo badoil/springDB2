317a62492974d3bffd8e8d589093b318b4950e95
oneal.oh <oneal.oh@kakaomobility.com>
2022-09-23 14:04:43 +0900

    update: ItemRepositoryV2/ItemQueryRepositoryV2

---
+++ src/main/java/hello/itemservice/config/V2Config.java
@@ -0,0 +1,40 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.ItemQueryRepositoryV2;
+import hello.itemservice.repository.jpa.ItemRepositoryV2;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import hello.itemservice.service.ItemServiceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class V2Config {
+
+    private final EntityManager em;
+
+    private final ItemRepositoryV2 itemRepositoryV2;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV2(itemRepositoryV2, itemQueryRepositoryV2());
+    }
+
+    @Bean
+    public ItemQueryRepositoryV2 itemQueryRepositoryV2() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+    //ItemRepository 는 테스트에서 사용하므로 여전히 필요하다.
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV3(em);
+    }
+}
