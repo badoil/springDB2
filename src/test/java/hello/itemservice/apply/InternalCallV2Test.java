d82b4611eb1aeea7f56712315b31c733644baf73
oneal.oh <oneal.oh@kakaomobility.com>
2022-09-23 17:22:54 +0900

    update: InternalCallV2Test, 내부호출 할 경우 aop 프록시 적용 안되는 케이스 해결

---
+++ src/test/java/hello/itemservice/apply/InternalCallV2Test.java
@@ -0,0 +1,90 @@
+package hello.itemservice.apply;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV2Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1Config {
+
+        @Bean
+        CallService callService() {
+            return new CallService(innerService());
+        }
+
+        @Bean
+        InternalService innerService() {
+            return new InternalService();
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class CallService {
+        private final InternalService internalService;
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internalService.internal();         // 기대와 다르게 internal() 에서 트랜잭션이 전혀 적용되지 않았다
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+
+    static  class InternalService {
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
