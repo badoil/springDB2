8f746fd78a1fb6bfbd7bd18a933b836c18a3d435
oneal.oh <oneal.oh@kakaomobility.com>
2022-09-23 23:19:50 +0900

    update: NotEnoughMoneyException

---
+++ src/test/java/hello/itemservice/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package hello.itemservice.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Slf4j
+@SpringBootTest
+class OrderServiceTest {
+
+//    @Autowired OrderService orderService;
+//
+//    @Autowired OrderRepository orderRepository;
+
+
+    @Test
+    void order() {
+    }
+}
