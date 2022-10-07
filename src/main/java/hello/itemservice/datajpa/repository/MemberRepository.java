d622b9e93b3e079e2e69ff4de663d8980f1002fa
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-07 16:11:49 +0900

    update: MemberRepositoryTest, testMethodQuery()

--- src/main/java/hello/itemservice/datajpa/repository/MemberRepository.java
+++ src/main/java/hello/itemservice/datajpa/repository/MemberRepository.java
@@ -1,11 +1,21 @@
 package hello.itemservice.datajpa.repository;
 
+import hello.itemservice.datajpa.dto.MemberDto;
 import hello.itemservice.datajpa.entity.Member;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+
+    @Query("select new hello.itemservice.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
