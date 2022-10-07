5c2be58fd8823d051bb6483b68f81b2b2876418d
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-07 15:39:26 +0900

    update: MemberRepositoryTest, findByUsernameAndAgeGreaterThan()

--- src/main/java/hello/itemservice/datajpa/entity/Member.java
+++ src/main/java/hello/itemservice/datajpa/entity/Member.java
@@ -36,6 +36,12 @@ public class Member {
         }
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
