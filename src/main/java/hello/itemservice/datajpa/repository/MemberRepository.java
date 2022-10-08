8fdc03f455eec14c3e5f8247c5bdef1a7f811d95
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-08 14:05:12 +0900

    update: MemberRepositoryTest, bulkUpdate()

--- src/main/java/hello/itemservice/datajpa/repository/MemberRepository.java
+++ src/main/java/hello/itemservice/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -23,4 +24,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
     Page<Member> findByAge(int age, Pageable pageable);
+
+    //이 옵션 없이 회원을 findById로 다시 조회하면 영속성 컨텍스트에 과거 값이 남아서 문제가 될 수 있다. 만약 다시 조회해야 하면 꼭 영속성 컨텍스트를 초기화 하자
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
