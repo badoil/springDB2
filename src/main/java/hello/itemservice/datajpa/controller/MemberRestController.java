a38981ca87deb71fd56cbcf6d9aee67bb513baf9
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-14 15:51:00 +0900

    update: MemberRestController, searchMemberV3

--- src/main/java/hello/itemservice/datajpa/controller/MemberRestController.java
+++ src/main/java/hello/itemservice/datajpa/controller/MemberRestController.java
@@ -4,7 +4,10 @@ package hello.itemservice.datajpa.controller;
 import hello.itemservice.datajpa.dto.MemberSearchCond;
 import hello.itemservice.datajpa.dto.MemberTeamDto;
 import hello.itemservice.datajpa.repository.MemberJpaRepository;
+import hello.itemservice.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,15 @@ public class MemberRestController {
 
     MemberJpaRepository memberJpaRepository;
 
+    MemberRepository memberRepository;
+
     @GetMapping("v2/members")
     public List<MemberTeamDto> search(MemberSearchCond condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCond condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
